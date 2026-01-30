d63be573c1b51884f43fffde20b968ac88f2a8a7
um3600 <umar.bin.riaz99@gmail.com>
2026-01-30 22:10:51 +0500

    update

---
+++ send_email.php
@@ -0,0 +1,33 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Replace this with your email
+    $to = "umar.bin.riaz99@gmail.com";
+
+    // Get form data
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $subject = htmlspecialchars($_POST['subject']);
+    $message = htmlspecialchars($_POST['message']);
+
+    // Email headers
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
+
+    // Email body
+    $body = "Name: $name\n";
+    $body .= "Email: $email\n";
+    $body .= "Subject: $subject\n";
+    $body .= "Message:\n$message\n";
+
+    // Send email
+    if (mail($to, $subject, $body, $headers)) {
+        echo "<script>alert('Message sent successfully!'); window.location.href='contact.html';</script>";
+    } else {
+        echo "<script>alert('Failed to send message. Please try again.'); window.location.href='contact.html';</script>";
+    }
+} else {
+    header("Location: contact.html");
+    exit();
+}
+?>
